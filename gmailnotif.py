fcdacf75d2c839f569e4961a7a10dfc03f64c0c4
xavier petit <nuxion@gmail.com>
2017-04-18 01:05:44 -0300

    separate in different files, finish de bash script

--- gmailnotif.py
+++ gmailnotif.py
@@ -1,69 +1,23 @@
 #!/usr/bin/env python3
-from __future__ import print_function
-import ipdb
-import httplib2
-import os
-import time
-
+#from __future__ import print_function
+#import ipdb
 from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
-import json
+#from oauth2client import client
+#from oauth2client import tools
+#from oauth2client.file import Storage
 import subprocess
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
-@profile
-def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+from files import loadFile, saveFile, get_credentials
 
 class Gmail:
-    @profile
-    def __init__(self):
-        
-        # Initializaation of the gmail api
-        credentials = get_credentials()
-        self.http = credentials.authorize(httplib2.Http())
+    """ Clase principal que hace la llamada a la api de gmail
+    y obtiene los ultimos 10 mails del INBOX. Los compara con la ultima lista obtenida y sacando la diferencia entre nuevos y viejos identifica si hay alertas. """
+    
+    def __init__(self, http):
         # Creo el servicio a usar, en este caso gmail
-        self.service = discovery.build('gmail', 'v1', http=self.http)
+        self.service = discovery.build('gmail', 'v1', http=http)
         # Inicializaciones propias de la clase
         self.newMails = []
         self.oldMails = loadFile()
-    @profile    
     def listMails(self, maxResults=10):
         """ Atraves del api de google obtengo los 10 ultimos mensajes 
         del INBOX. 
@@ -79,7 +33,6 @@ class Gmail:
             #messages.extend(response['messages'])
             for m in response['messages']:
                 self.newMails.append(m['id'])
-    @profile
     def mailbymail(self, listIDS):
         listMails = []
         for m in listIDS:
@@ -96,33 +49,14 @@ class Gmail:
             listMails.append(dictEmail)
         return listMails
                 
-    @profile    
     def compareMsgs(self):
         """ Verify the oldMails with the newMails, and return only the unique element. """
         #temp3 = [x for x in self.oldMails if x not in self.newMails]
         temp3 = [x for x in self.newMails if x not in self.oldMails]
         return temp3 
-    @profile
     def saveNew(self):
         saveFile(self.newMails, pathSTR="lastids.txt")
         
-        
-def loadFile(pathSTR="lastids.txt"):
-    listIDS=[]
-    with open (pathSTR, "r") as fileids:
-        for line in fileids:
-            # se ejecuta rstrip() para remover el '\n'
-            listIDS.append(line.rstrip())
-            #ipdb.set_trace()
-    return listIDS 
-def saveFile(savedata, pathSTR="newids.txt"):
-    """ Method that recive a listvalue and write it to the file. 
-    `savedata` []
-    `pathSTR` string. """
-    with open (pathSTR, "w") as filewrite:
-        for x in savedata:
-            filewrite.write(x+"\n")
-        #filewrite(savedata)
 def sendNotifications(allData):
     #cmd = "/home/nuxion/scripts/notify.sh"
     cmd = "/usr/bin/notify-send -u critical"
@@ -134,8 +68,12 @@ def sendNotifications(allData):
         subprocess.Popen([command], shell=True)
     
 if __name__ == "__main__":
+    import time
+    import httplib2
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
     while True:
-        listaMails = Gmail()
+        listaMails = Gmail(http)
         listaMails.listMails()
         newElements = listaMails.compareMsgs()
         # if i have new mails then
@@ -148,7 +86,6 @@ if __name__ == "__main__":
         #del listaMails
         #del newElements
         time.sleep(30)
-        
         
               
         
---
+++ gmailnotify.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+export DISPLAY=:0
+BASEDIR="/home/nuxion/Projects/gmail/gmail" 
+MAILDIR=$BASEDIR"/mails/"
+
+var=$1
+notify="/usr/bin/notify-send"
+PYTH="/usr/bin/python3"
+
+function check_ok (){
+	
+	if [ ! -f $BASEDIR/lastids.txt ]; then
+		echo "123456" > $BASEDIR/lastids.txt
+	else
+		$PYTH $BASEDIR/main.py
+	fi
+	if [ $# -eq 0 ]; then
+		var="S"
+	fi
+}
+
+### MAIN ### 
+# Chequeo todas las condiciones
+check_ok
+if [ "$(ls -A $MAILDIR)" ]; then
+	if [ $var=="F" ];then
+		$notify -u critical "Gmail Notification v0.02" "$( cat $MAILDIR/mail.txt)"
+	else
+		for x in `ls $MAILDIR | grep -v mail.txt`
+		do
+			$notify -u critical "Gmail Notification v0.02" "$(cat $MAILDIR$x)"
+		done
+	fi
+	rm $MAILDIR/*.txt
+else 
+	echo "No hay mails nuevos"
+fi
